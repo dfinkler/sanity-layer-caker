0d60ae520aebe9b3c3675b894246554092bbd184
Dan Finkler <dan@joinbonsai.co>
2025-06-23 17:19:04 -0400

    sanity layer caker demo

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "@/app/globals.css"
+import { SanityLive } from "@/sanity/lib/live";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <SanityLive />
       </body>
     </html>
   )
