ee60e11c6b0bfd300a99d2426188a4980573428d
Dan Finkler <dan@joinbonsai.co>
2025-06-24 12:04:57 -0400

    cleaned out commented code

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -1,38 +1,21 @@
 import { sanityFetch } from "@/sanity/lib/live";
-import { POSTS_QUERY } from '@/sanity/lib/queries'
-import { PostCard } from '@/components/PostCard'
-import { Title } from '@/components/Title'
-
-// export default async function PageFn() {
-//   const {data: posts} = await sanityFetch({query: POSTS_QUERY});
-
-//   return (
-//     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
-//       <Title>Post Index</Title>
-//       <div className="flex flex-col gap-24 py-12">
-//         {
-//         // eslint-disable-next-line @typescript-eslint/no-explicit-any 
-//         posts.map((post:any) => (
-//           <PostCard key={post._id} {...post} />
-//         ))}
-//       </div>
-//     </main>
-//   )
-// }
-
+import { POSTS_QUERY } from "@/sanity/lib/queries";
+import { PostCard } from "@/components/PostCard";
+import { Title } from "@/components/Title";
 
 const Page = async () => {
-    const {data: posts} = await sanityFetch({query: POSTS_QUERY});
+  const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
 
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <Title>Post Index</Title>
       <div className="flex flex-col gap-24 py-12">
         {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any 
-        posts.map((post:any) => (
-          <PostCard key={post._id} {...post} />
-        ))}
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          posts.map((post: any) => (
+            <PostCard key={post._id} {...post} />
+          ))
+        }
       </div>
     </main>
   );
