127c9c3f1052d5b89781048ba835dbfd8b7ddf0c
Dan Finkler <dan@joinbonsai.co>
2025-06-23 20:42:21 -0400

    fix unused variable

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -3,12 +3,8 @@ import Link from "next/link";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch } from "@/sanity/lib/live";
 
-const options = { next: { revalidate: 60 } };
-
 export default async function Page() {
-
   const { data: posts } = await sanityFetch({query: POSTS_QUERY})
-
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <h1 className="text-4xl font-bold">Post index</h1>
