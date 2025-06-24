1ecfe36116b5009463ca1b50c8bc5a664194f8d1
Dan Finkler <dan@joinbonsai.co>
2025-06-24 10:58:16 -0400

    rewrote to use more functional style

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -3,8 +3,26 @@ import { POSTS_QUERY } from '@/sanity/lib/queries'
 import { PostCard } from '@/components/PostCard'
 import { Title } from '@/components/Title'
 
-export default async function Page() {
-  const {data: posts} = await sanityFetch({query: POSTS_QUERY});
+// export default async function PageFn() {
+//   const {data: posts} = await sanityFetch({query: POSTS_QUERY});
+
+//   return (
+//     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
+//       <Title>Post Index</Title>
+//       <div className="flex flex-col gap-24 py-12">
+//         {
+//         // eslint-disable-next-line @typescript-eslint/no-explicit-any 
+//         posts.map((post:any) => (
+//           <PostCard key={post._id} {...post} />
+//         ))}
+//       </div>
+//     </main>
+//   )
+// }
+
+
+const Page = async () => {
+    const {data: posts} = await sanityFetch({query: POSTS_QUERY});
 
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
@@ -17,5 +35,7 @@ export default async function Page() {
         ))}
       </div>
     </main>
-  )
-}
+  );
+};
+
+export default Page;
